292e98e946362680933da178b0ba5b1d506f4900
Duc <levietduc2000cb@gmail.com>
2024-08-19 15:01:15 +0700

    Làm thêm hiệu ứng đếm số và lướt từ trái qua phải, từ phải qua trái

---
+++ js/index.js
@@ -0,0 +1,73 @@
+//Đếm số
+function countUp(element, start, end, duration) {
+    const COUNT_NUMBER = 30;
+    let range = end - start;
+    let stepTime = COUNT_NUMBER;
+    let increment = Math.round (end / COUNT_NUMBER);
+    let current = start;
+    let timer = setInterval(function() {
+        current += increment;
+        element.textContent = current;
+        if (increment > 0 && current >= end || increment < 0 && current <= end) {
+            clearInterval(timer);
+            element.textContent = end; // Đảm bảo giá trị cuối cùng được hiển thị chính xác
+        }
+    }, stepTime);
+}
+document.addEventListener('DOMContentLoaded', function () {
+    let counters = document.querySelectorAll('.counter');
+    let observer = new IntersectionObserver(function(entries) {
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+                let element = entry.target;
+                let endValue = parseInt(element.getAttribute('data-count'));
+                countUp(element, 0, endValue, 2000); // Sử dụng hàm countUp
+                observer.unobserve(element); // Ngừng quan sát sau khi số đã chạy
+            }
+        });
+    });
+    counters.forEach(function(counter) {
+        observer.observe(counter);
+    });
+
+    //SLide trang Home
+    let slideContentWrapper = document.querySelector(".slide_content_wrapper");
+    let slideImageWrapper = document.querySelector(".slide_image_wrapper");
+    let arrowLeft = document.querySelector("#arrow_left");
+    let arrowRight = document.querySelector("#arrow_right");
+    arrowLeft.addEventListener("click",(e)=>{
+        let slideContent = document.querySelectorAll(".slide_content");
+        let slideImage = document.querySelectorAll(".slide_image");
+        slideContentWrapper.append(slideContent[0]);
+        slideImageWrapper.append(slideImage[0]);
+    });
+    arrowRight.addEventListener("click",(e)=>{
+        let slideContent = document.querySelectorAll(".slide_content");
+        let slideImage = document.querySelectorAll(".slide_image");
+        slideContentWrapper.prepend(slideContent[slideContent.length - 1]);
+        slideImageWrapper.prepend(slideImage[slideImage.length - 1]);
+    });
+
+    //Lướt từ trái sang phải và từ phải sang trái
+    let animationSwipeRights = document.querySelectorAll('.animation_swipe_right');
+    let animationSwipeLefts = document.querySelectorAll('.animation_swipe_left');
+    let observerAnimationSwipe = new IntersectionObserver(function(entries) {
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+                let element = entry.target;
+                if(element.classList.contains("animation_swipe_right")){
+                    element.classList.add("swipe_right_to_left");
+                }else{
+                    element.classList.add("swipe_left_to_tight");
+                }
+                observerAnimationSwipe.unobserve(element);
+            }
+        });
+    });
+    animationSwipeRights.forEach(function(animationSwipe) {
+        observerAnimationSwipe.observe(animationSwipe);
+    });
+    animationSwipeLefts.forEach(function(animationSwipe) {
+        observerAnimationSwipe.observe(animationSwipe);
+    });
+});
